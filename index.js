56ddd679a5075b75024bdd4038ee07466b5a8390
julian carreno <julianctsistemas@gmail.com>
2023-10-12 20:27:17 -0500

    actualizacion

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const sequelize = require('./src/config/sequelize');
 const pedidosRoutes = require('./src/routes/pedidosRoutes');
 
 
-const app = express();  
+const app = express();
 
 const PORT = process.env.PORT || 8080;
-app.use(express.json());  
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 (async () => {
   try {
