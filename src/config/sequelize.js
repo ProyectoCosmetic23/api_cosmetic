fb5581b83a2dbb589048db3f71f7f5aeca1b0b44
alejoc0314 <alejoc0314@gmail.com>
2023-11-06 14:00:58 -0500

    IP Update

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('cosmetic_db', 'postgres', 'cosmetic1234', {
-  host: '127.0.0.1',
+  host: '186.84.84.33',
   dialect: 'postgres',
 });
 
