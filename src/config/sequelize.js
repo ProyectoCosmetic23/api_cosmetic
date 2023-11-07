60f651c9551b3dc8eb8ebf4d59d8f1b7ce3259a7
alejoc0314 <alejoc0314@gmail.com>
2023-11-07 15:46:58 -0500

    Fix db

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('cosmetic_db_qyvo', 'adminuser', '8OIXi0HzBZ9hdicPW4abzHM900CCL5wW', {
-  host: 'dpg-cl59km182rpc73fraehg-a.ohio-postgres.render.com',
+  host: 'dpg-cl5a062l7jac73cfphr0-a.ohio-postgres.render.com',
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // Habilita SSL/TLS
+    },
+  },
 });
 
 module.exports = sequelize;
