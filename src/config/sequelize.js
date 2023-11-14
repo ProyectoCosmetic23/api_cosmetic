b652622722d244a1aa790e835ae21d6292fe333d
alejoc0314 <alejoc0314@gmail.com>
2023-11-14 13:03:52 -0500

    Fix api online

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,23 +1,21 @@
 const { Sequelize } = require('sequelize');
 
 
-const sequelize = new Sequelize('cosmetic_db', 'postgres', 'cosmetic1234', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
-
-module.exports = sequelize;
-
-// const { Sequelize } = require('sequelize');
-
-// const sequelize = new Sequelize('cosmetic_db_qyvo', 'adminuser', '8OIXi0HzBZ9hdicPW4abzHM900CCL5wW', {
-//   host: 'dpg-cl5a062l7jac73cfphr0-a.ohio-postgres.render.com',
+// const sequelize = new Sequelize('cosmetic_db', 'postgres', 'cosmetic1234', {
+//   host: 'localhost',
 //   dialect: 'postgres',
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//     },
-//   },
 // });
 
 // module.exports = sequelize;
+
+const sequelize = new Sequelize('cosmetic_db_qyvo', 'adminuser', '8OIXi0HzBZ9hdicPW4abzHM900CCL5wW', {
+  host: 'dpg-cl5a062l7jac73cfphr0-a.ohio-postgres.render.com',
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
+});
+
+module.exports = sequelize;
