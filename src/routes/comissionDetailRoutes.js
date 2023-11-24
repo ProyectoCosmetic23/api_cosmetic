bab0adbb7397e3dd832f3e10022cfdc15f837d6a
Jotechno <jstamayotoro@hotmail.com>
2023-11-23 23:26:02 -0500

    pagos

--- src/routes/comissionDetailRoutes.js
+++ src/routes/comissionDetailRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const comissionDetailController = require ('../controllers/Comissions/comissionDetailController');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
 
 router.get('/detailComs',validarJWT, comissionDetailController.getAllDetails);
 router.get('/commissions/detailComs/:id',validarJWT, comissionDetailController.getDetailComsById);
