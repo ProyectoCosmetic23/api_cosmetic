5e5c91aa3336bf33ae23383c3eccb9f0cd214b64
MarcelaM18 <marcelamorales1808@gmail.com>
2023-11-28 15:52:37 -0500

    dto compra

---
+++ src/dtos/purchaseDto.js
@@ -0,0 +1,20 @@
+const purchaseDTO = {
+  id_purchase:0,
+
+  id_provider:0,
+  
+
+  invoice_number:'',
+
+  purchase_date:'',
+
+  record_date_purchase:'',
+
+  total_purchase:'',
+
+  state_purchase:null,
+
+  observation_purchase:'',
+}
+
+module.exports = purchaseDTO;
