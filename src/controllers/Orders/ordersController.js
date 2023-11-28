d32775b5d0df40fbf131b8e81dda14ecbce533b0
ProyectoCosmetic23 <147460196+ProyectoCosmetic23@users.noreply.github.com>
2023-11-28 17:58:57 -0500

    commit

--- src/controllers/Orders/ordersController.js
+++ src/controllers/Orders/ordersController.js
@@ -5,7 +5,7 @@ const Order_Detail = require("../../models/order_detail");
 const Sales = require("../../models/sales");
 const Sale_Detail = require("../../models/sale_detail");
 const Products = require("../../models/products");
-const Comissions = require("../../models/commissions");
+const { updateComissionsFromSales } = require ('../Comissions/comissionController');
 
 // -------------- INICIO: Función para para obtener último N°Pedido -------------- //
 
