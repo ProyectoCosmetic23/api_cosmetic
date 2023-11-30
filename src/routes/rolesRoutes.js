45af77a67d9e78593210590f4e698dfd1896216c
alejoc0314 <alejoc0314@gmail.com>
2023-11-30 15:27:28 -0500

    Fix roles

--- src/routes/rolesRoutes.js
+++ src/routes/rolesRoutes.js
@@ -5,6 +5,7 @@ const { validarJWT } = require('../middlewares/validar-jwt');
 
 router.get('/roles', validarJWT, rolesController.getAllRoles);
 router.get('/roles/:id', validarJWT, rolesController.getRoleById);
+router.get('/api/validate-role/:name', validarJWT, rolesController.validateRoleName);
 router.post('/roles', validarJWT, rolesController.createRole);
 router.put('/roles/update/:id', validarJWT, rolesController.updateRole);
 router.put('/roles/updateStatus/:id', validarJWT, rolesController.updateRoleStatus);
