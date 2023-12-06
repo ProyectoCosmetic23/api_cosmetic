1eb3c0795e9ed11393e336688b5f9cf367b78bdd
alejoc0314 <alejoc0314@gmail.com>
2023-12-06 06:43:45 -0500

    Fix orders

--- src/controllers/Orders/ordersController.js
+++ src/controllers/Orders/ordersController.js
@@ -340,8 +340,8 @@ function handleError(res, error, errorMessage) {
 // Función para anular pedidos por ID
 async function anulateOrderById(req, res) {
   const { id } = req.params;
-  const { observation } = req.body;
-  console.log(observation);
+  const { observation } = req.body.value;
+  console.log("msg anular: ", observation);
   var order_state = "Anulado";
   try {
     const order = await Orders.findByPk(id);
