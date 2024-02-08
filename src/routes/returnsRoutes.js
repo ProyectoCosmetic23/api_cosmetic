2ceaa2a016ad77db1af944e916283c4e2f4f57c0
marianagrasi <marianagranados201@gmail.com>
2024-02-08 14:14:01 -0500

    Devoluciones

--- src/routes/returnsRoutes.js
+++ src/routes/returnsRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const returnsController = require ('../controllers/Returns/returnsController');
-const { validarJWT } = require('../middlewares/validar-jwt');
 
 //Procesar una devolución
 router.post('/processReturn', returnsController.processReturn);
@@ -11,11 +10,5 @@ router.get('/returns_detail/:id', returnsController.getReturnById )
 
 //Obterner un pedido
 router.get('/returns/:id', returnsController.getOrderById )
-
-//devolver producto
-router.put('/returns/retire/:id', returnsController.retireProduct);
-
-//anular pedido
-router.put('/returns/anulate/:id', validarJWT, returnsController.anulateOrderByIdR);
 
 module.exports = router;
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,27 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const usersController = require('../controllers/Users/usersController');
-const { validarJWT } = require('../middlewares/validar-jwt');
+const usersController = require("../controllers/Users/usersController");
+const { validarJWT } = require("../middlewares/validar-jwt");
 
 // Rutas de autenticación
-router.get('/ruta-protegida', validarJWT, (req, res) => {
-    // Lógica para la ruta protegida
-    res.json({ mensaje: 'Esta es una ruta protegida' });
-  });
-router.get('/users', usersController.getAllUsers);
-router.get('/users/:id', usersController.getUserById);
-router.post('/users', usersController.createUser);
-router.put('/users/:id',validarJWT, usersController.updateUser);
-router.put('/users/state/:id', usersController.updateUserState);
-router.post('/users/login' , usersController.loginUser);
-router.post('/users/recover', usersController.forgotPassword);
-router.post('/change-password/:token', usersController.changePassword);
+router.get("/ruta-protegida", validarJWT, (req, res) => {
+  // Lógica para la ruta protegida
+  res.json({ mensaje: "Esta es una ruta protegida" });
+});
+router.get("/users", usersController.getAllUsers);
+router.get("/users/:id", usersController.getUserById);
+router.post("/users", usersController.createUser);
+router.put("/users/:id", validarJWT, usersController.updateUser);
+router.put("/users/state/:id", usersController.updateUserState);
+router.post("/users/login", usersController.loginUser);
+router.post("/users/recover", usersController.forgotPassword);
+router.post("/change-password/:token", usersController.changePassword);
 //Ruta para buscar el empleado y obtener su correo
-router.get('/Users/employeeByCard/:id_card_employee', usersController.employeeByCard);
+router.get(
+  "/Users/employeeByCard/:id_card_employee",
+  usersController.employeeByCard
+);
 
 //ruta para validar usuario existente
-router.get('/Users-check-empleado', usersController.checkEmployeeAvailability);
-
-
+// router.get('/Users-check-empleado', usersController.checkEmployeeAvailability);
 
 module.exports = router;
