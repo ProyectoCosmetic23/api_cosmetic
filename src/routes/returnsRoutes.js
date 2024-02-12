d1828e4d598f4eb0dc2c00a4df7f9f0b3aaeaa68
marianagrasi <marianagranados201@gmail.com>
2024-02-12 17:45:58 -0500

    Se crea validacion para la creacion unica de usuario para un empleado

--- src/routes/returnsRoutes.js
+++ src/routes/returnsRoutes.js
@@ -9,12 +9,6 @@ router.post('/processReturn', returnsController.processReturn);
 router.get('/returns_detail/:id', returnsController.getReturnById );
 
 //Obterner un pedido
-<<<<<<< HEAD
-router.get('/returns/:id', returnsController.getOrderById )
-
-module.exports = router;
-=======
 router.get('/returns/:id', returnsController.getOrderById );
 
 module.exports = router;
->>>>>>> 8f73f51771411962d79b6daf449a101c1f81615d
