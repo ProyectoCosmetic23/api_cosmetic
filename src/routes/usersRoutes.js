14209e5ef90516a32a3c9058375a58b5af27070a
alejoc0314 <alejoc0314@gmail.com>
2024-03-15 06:43:53 -0500

    Main commit

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -4,21 +4,6 @@ const usersController = require("../controllers/Users/usersController");
 const { validarJWT } = require("../middlewares/validar-jwt");
 
 // Rutas de autenticación
-<<<<<<< HEAD
-router.get('/ruta-protegida', validarJWT, (req, res) => {
-    // Lógica para la ruta protegida
-    res.json({ mensaje: 'Esta es una ruta protegida' });
-  });
-router.get('/users', usersController.getAllUsers);
-router.get('/users/:id', usersController.getUserById);
-router.post('/users', usersController.createUser);
-router.put('/users/:id',validarJWT, usersController.updateUser);
-router.put('/users/state/:id', usersController.updateUserState);
-router.post('/users/login' , usersController.loginUser);
-router.post('/users/recover', usersController.forgotPassword);
-router.post('/change-password/:token', usersController.changePassword);
-
-=======
 router.get("/ruta-protegida", validarJWT, (req, res) => {
   // Lógica para la ruta protegida
   res.json({ mensaje: "Esta es una ruta protegida" });
@@ -31,7 +16,6 @@ router.put("/users/state/:id", usersController.updateUserState);
 router.post("/users/login", usersController.loginUser);
 router.post("/users/recover", usersController.forgotPassword);
 router.post("/change-password/:token", usersController.changePassword);
->>>>>>> d1828e4d598f4eb0dc2c00a4df7f9f0b3aaeaa68
 //Ruta para buscar el empleado y obtener su correo
 router.get(
   "/Users/employeeByCard/:id_card_employee",
